41839a866580cb24d55d78ee0dd40390dbedf4dd
Stephen-Lail <sml6042@uncw.edu>
2024-09-30 15:33:16 -0400

    Fix code

--- SOS_Argon/src/SOS_Argon.cpp
+++ SOS_Argon/src/SOS_Argon.cpp
@@ -1,6 +1,16 @@
-// We define MY_LED to be the LED that we want to blink.
+/* 
+ * Project myProject
+ * Author: Your Name
+ * Date: 
+ * For comprehensive documentation and examples, please visit:
+ * https://docs.particle.io/firmware/best-practices/firmware-template/
+ */
+
+// Include Particle Device OS APIs
 #include "Particle.h"
 
+// We define MY_LED to be the LED that we want to blink.
+//
 // In this tutorial, we're using the blue D7 LED (next to D7 on the Photon
 // and Electron, and next to the USB connector on the Argon and Boron).
 const pin_t MY_LED = D7;
@@ -29,34 +39,16 @@ void setup()
 void loop()
 {
 	// Turn on the LED
-	digitalWrite(MY_LED, HIGH);  //long blink 
-  delay(400);                       
-  digitalWrite(MY_LED, LOW); 
-  delay(400);                      
-  digitalWrite(MY_LED, HIGH);  //long blink
-  delay(400); 
-  digitalWrite(MY_LED, LOW);   
-  delay(400);
-  digitalWrite(MY_LED, HIGH);  //long blink
-  delay(400);
-  digitalWrite(MY_LED, LOW);   
-  delay(400); 
-  digitalWrite(MY_LED, HIGH);  //long blink
-  delay(400);                       
-  digitalWrite(MY_LED, LOW);    
-  delay(200);                      
-  digitalWrite(MY_LED, HIGH);  //short blink
-  delay(200); 
-  digitalWrite(MY_LED, LOW);
-  delay(200);
-  digitalWrite(MY_LED, HIGH);  //short blink
-  delay(200);
-  digitalWrite(MY_LED, LOW);
-  delay(200);
-  digitalWrite(MY_LED, HIGH);  //short blink
-  delay(200);
-  digitalWrite(MY_LED, LOW);
-}
+	digitalWrite(MY_LED, HIGH);
+
+	// Leave it on for one second
+	delay(3s);
+
+	// Turn it off
+	digitalWrite(MY_LED, LOW);
+
+	// Wait one more second
+	delay(1s);
 
 	// And repeat!
 }
